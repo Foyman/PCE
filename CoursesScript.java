c45b7a542e7c370f2accaa5f7ce9b7f1852bc5ed
Tim Stoddard <tim.stoddard2@gmail.com>
2017-09-22 14:21:52 -0700

    clean up logic for courses script

--- CoursesScript.java
+++ CoursesScript.java
@@ -12,7 +12,6 @@ public class CoursesScript
         PrintStream out = new PrintStream(new FileOutputStream("courses.txt"));
         System.setOut(out);
         getDepartments(in);
-
         in.close();
     }
 
@@ -23,52 +22,68 @@ public class CoursesScript
         {
             if (inputLine.compareTo("<table>") == 0)
             {
-                while ((inputLine = in.readLine()).compareTo("</table>") != 0)
-                {
-                    String department = inputLine.substring(
-                        inputLine.indexOf("(") + 1, inputLine.indexOf(")"));
-                    System.out.println("~~~ " + department + " ~~~");
-                    getCourses(department, getDepartmentURL(inputLine));
-                    System.out.println("\n");
-                }
+                break;
             }
+        }
+
+        while ((inputLine = in.readLine()).compareTo("</table>") != 0)
+        {
+            String department = inputLine.substring(
+                inputLine.indexOf("(") + 1, inputLine.indexOf(")"));
+            System.out.println("*** " + department + " ***");
+            getCourses(getDepartmentURL(inputLine));
+            System.out.println("\n");
         }
     }
 
-    public static String getDepartmentURL(String inputLine)
+    private static String getDepartmentURL(String inputLine)
     {
-        String link = inputLine.substring(inputLine.indexOf("href=\"") + 6, inputLine.indexOf("\" "));
-        link = "http://catalog.calpoly.edu" + link;
-        return link;
+        String link = "";
+        Matcher match = Pattern
+            .compile("href=\"([/a-z]+)\"")
+            .matcher(inputLine);
+
+        if (match.find())
+        {
+            link = match.group(1);
+        }
+
+        return "http://catalog.calpoly.edu" + link;
     }
 
-    public static void getCourses(String department, String link) throws Exception
+    private static void getCourses(String link) throws Exception
     {
-        String courseNum, inputLine;
+        String inputLine;
         URL site = new URL(link);
         BufferedReader in = new BufferedReader(
-                new InputStreamReader(site.openStream()));
+            new InputStreamReader(site.openStream()));
 
         while ((inputLine = in.readLine()) != null)
         {
             if (inputLine.contains("courseblocktitle"))
             {
-                courseNum = inputLine.substring(inputLine.indexOf(department) + department.length() + 6, inputLine.indexOf("."));
-                Pattern p = Pattern.compile("([A-Z]+)&#160;(\\d+)\\. (.*)\\.");
-                Matcher m = p.matcher(inputLine);
-                if (m.find())
-                {
-                    String courseNumber = m.group(2);
-                    String courseName = m.group(3);
-                    System.out.println(department + " " + courseNumber + ": " + courseName);
-                }
-                else
-                {
-                    System.err.println("Parsing failed: " + inputLine);
-                }
+                extractCourseData(inputLine);
             }
         }
-        
         in.close();
+    }
+
+    private static void extractCourseData(String inputLine)
+    {
+        Matcher match = Pattern
+            .compile("([A-Z]+)&#160;(\\d+)\\. (.*)\\.")
+            .matcher(inputLine);
+
+        if (match.find())
+        {
+            String department = match.group(1);
+            String courseNumber = match.group(2);
+            String courseName = match.group(3);
+            System.out.println(department + " " + courseNumber + ": " + courseName);
+        }
+        else
+        {
+            System.err.println("Parsing failed: " + inputLine);
+        }
     }
 }
