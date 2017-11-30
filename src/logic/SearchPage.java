3ea3c107bac3d975fa2758a7a1f947a38d7bb018
Collin <Collin@129.65.48.247>
2017-11-29 21:32:23 -0800

    Last SonarQube Fixes!

--- src/logic/SearchPage.java
+++ src/logic/SearchPage.java
@@ -41,86 +41,92 @@ public class SearchPage
                 break;
         }
 
-        // Adding JTable for Search Data
-        final JTable table = new JTable();
-        table.setModel(new DefaultTableModel(rowData, columnNames)
-        {
-
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public boolean isCellEditable(int row, int col)
-            {
-                return false;
-            }
-        });
-
-        // Changing the Table Header
-        JTableHeader tableHeader = table.getTableHeader();
-        tableHeader.setFont(new Font("Verdana", Font.BOLD, 30));
-        tableHeader.setSize(tableHeader.getWidth(), 50);
-
-        // Adds listener to get when a course is double clicked
-        table.addMouseListener(new MouseListener()
-        {
-            //@Override
-            public void mouseClicked(MouseEvent e)
-            {
-                if (e.getClickCount() == 2)
-                {
-                    // VERIFIES THAT IT IS CLICKING ON THE ACTUAL COURSE
-                    String selectedCourse = courses.get(table.getSelectedRow()).getName();
-                    String dept = selectedCourse.split(" ")[0];
-                    String courseNum = selectedCourse.split(" ")[1];
-                    Home.searchForReview(dept, courseNum, selectedCourse);
-
-                }
-            }
-
-            //@Override
-            public void mouseEntered(MouseEvent arg0)
-            {
-                return;
-            }
-
-            //@Override
-            public void mouseExited(MouseEvent arg0)
-            {
-                return;
-            }
-
-            //@Override
-            public void mousePressed(MouseEvent arg0)
-            {
-                return;
-            }
-
-            //@Override
-            public void mouseReleased(MouseEvent arg0)
-            {
-                return;
-            }
-
-        });
-        table.setRowHeight(40);
-        table.setFont(new Font("Verdana", Font.PLAIN, 20));
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
-
-        // Makes table scroll-able
-        JScrollPane scroll = new JScrollPane(table);
-
-        // Everything for footer below
-        JLabel footText = new JLabel("© 2017 Polyratings Course Edition");
-        footText.setForeground(Color.WHITE);
-        footer.setBackground(new Color(7, 88, 64));
-        footer.add(footText);
-
-        // All panels into frame
-        panels.add(new JComponentWithLayout(header, BorderLayout.NORTH));
-        panels.add(new JComponentWithLayout(scroll, BorderLayout.CENTER));
-        panels.add(new JComponentWithLayout(footer, BorderLayout.SOUTH));
+        createFrameHelper(courses, columnNames, rowData, footer, header, panels);
 
         return panels;
+    }
+    
+    public static void createFrameHelper(final List<Course> courses, Object[] columnNames, Object[][] rowData,
+   		 final JPanel footer, final JPanel header, List<JComponentWithLayout> panels)
+    {
+   	// Adding JTable for Search Data
+       final JTable table = new JTable();
+       table.setModel(new DefaultTableModel(rowData, columnNames)
+       {
+
+           private static final long serialVersionUID = 1L;
+
+           @Override
+           public boolean isCellEditable(int row, int col)
+           {
+               return false;
+           }
+       });
+
+       // Changing the Table Header
+       JTableHeader tableHeader = table.getTableHeader();
+       tableHeader.setFont(new Font("Verdana", Font.BOLD, 30));
+       tableHeader.setSize(tableHeader.getWidth(), 50);
+
+       // Adds listener to get when a course is double clicked
+       table.addMouseListener(new MouseListener()
+       {
+           //@Override
+           public void mouseClicked(MouseEvent e)
+           {
+               if (e.getClickCount() == 2)
+               {
+                   // VERIFIES THAT IT IS CLICKING ON THE ACTUAL COURSE
+                   String selectedCourse = courses.get(table.getSelectedRow()).getName();
+                   String dept = selectedCourse.split(" ")[0];
+                   String courseNum = selectedCourse.split(" ")[1];
+                   Home.searchForReview(dept, courseNum, selectedCourse);
+
+               }
+           }
+
+           //@Override
+           public void mouseEntered(MouseEvent arg0)
+           {
+               return;
+           }
+
+           //@Override
+           public void mouseExited(MouseEvent arg0)
+           {
+               return;
+           }
+
+           //@Override
+           public void mousePressed(MouseEvent arg0)
+           {
+               return;
+           }
+
+           //@Override
+           public void mouseReleased(MouseEvent arg0)
+           {
+               return;
+           }
+
+       });
+       table.setRowHeight(40);
+       table.setFont(new Font("Verdana", Font.PLAIN, 20));
+       table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+
+       // Makes table scroll-able
+       JScrollPane scroll = new JScrollPane(table);
+
+       // Everything for footer below
+       JLabel footText = new JLabel("© 2017 Polyratings Course Edition");
+       footText.setForeground(Color.WHITE);
+       footer.setBackground(new Color(7, 88, 64));
+       footer.add(footText);
+
+       // All panels into frame
+       panels.add(new JComponentWithLayout(header, BorderLayout.NORTH));
+       panels.add(new JComponentWithLayout(scroll, BorderLayout.CENTER));
+       panels.add(new JComponentWithLayout(footer, BorderLayout.SOUTH));
     }
 
     /**
