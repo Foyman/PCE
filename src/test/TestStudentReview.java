e9c89de584b35aa27de229babc4bf4b59e152d39
JakeVz <jakevz122@gmail.com>
2017-11-29 20:57:17 -0800

    Fixed Test class to comply with sonar and whatnot

--- src/test/TestStudentReview.java
+++ src/test/TestStudentReview.java
@@ -15,17 +15,44 @@ import org.junit.jupiter.api.Test;
 public class TestStudentReview
 {
 
+	private static String string1 = "THIS IS MY REVIEW";
+	private static String string2 = "Course";
+    private static StudentReview r = new StudentReview(4.5, 2.5, 3.5, "A", string1, string2);
+
+	
     @Test
-    public void testConstructor()
+    public void testConstructor0()
     {
-        StudentReview r = new StudentReview(4.5, 2.5, 3.5, "A", "THIS IS MY REVIEW", "Course");
         assertEquals(4.5, r.getCriteria(1), .0);
-        assertEquals(2.5, r.getCriteria(2), .0);
-        assertEquals(3.5, r.getCriteria(3), .0);
-        assertEquals("A", r.getGrade());
-        assertEquals("THIS IS MY REVIEW", r.getReview());
-        assertEquals("Course", r.getCourse());
     }
+    
+    @Test
+    public void testConstructor1()
+    {
+        assertEquals(2.5, r.getCriteria(2), .0);
+    }
+    @Test
+    public void testConstructor2()
+    {
+        assertEquals(3.5, r.getCriteria(3), .0);
+    }
+    @Test
+    public void testConstructor3()
+    {
+        assertEquals("A", r.getGrade());
+    }
+    @Test
+    public void testConstructor4()
+    {
+        assertEquals(string1, r.getReview());
+    }
+      
+    @Test
+    public void testConstructor5()
+    {
+        assertEquals(string2, r.getCourse());
+    }
+    
 
     @Test
     public void testEmptyConstructor()
