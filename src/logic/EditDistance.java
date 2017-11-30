3ebc83889cbeabdc350cd6b44981bf63890cd240
Collin <Collin@129.65.48.247>
2017-11-29 20:30:21 -0800

    Fixed Home.java and EditDistance.java (removed Type) SonarQube

--- src/logic/EditDistance.java
+++ src/logic/EditDistance.java
@@ -4,74 +4,65 @@ import java.util.Collections;
 
 public class EditDistance
 {
-    private EditDistance()
-    {
+	private EditDistance()
+	{
 
-    }
+	}
 
-    /**
-     * Sorts the Course List in Search.java based on the Edit Distance between each
-     * course name and the search string.
-     * 
-     * @param search - The course name to search for
-     */
-    public static void sortList(String search, Type t)
-    {
-        CourseComparator compare = new CourseComparator();
-        search = search.toLowerCase();
-        if (t == Type.NAME)
-        {
-            for (Course c : Search.getCourses())
-            {
-                c.setDistance(getDistance(search, c.getName()));
-            }
-        } else if (t == Type.DESCRIPTION)
-        {
-            for (Course c : Search.getCourses())
-            {
-                c.setDistance(getDistance(search, c.getDescription()));
-            }
-        }
-        Collections.sort(Search.getCourses(), compare);
-    }
+	/**
+	 * Sorts the Course List in Search.java based on the Edit Distance between each
+	 * course name and the search string.
+	 * 
+	 * @param search - The course name to search for
+	 */
+	public static void sortList(String search)
+	{
+		CourseComparator compare = new CourseComparator();
+		search = search.toLowerCase();
+		for (Course c : Search.getCourses())
+		{
+			c.setDistance(getDistance(search, c.getName()));
+		}
+		Collections.sort(Search.getCourses(), compare);
+	}
 
-    /**
-     * Returns the Edit Distance of the two strings
-     * 
-     * @param s1 - First String to compare
-     * @param s2 - Second String to compare
-     * @return Edit Distance of the two strings
-     */
-    public static int getDistance(String s1, String s2)
-    {
-        int len1 = s1.length() + 1;
-        int len2 = s2.length() + 1;
-        int[][] ed = new int[len1][len2];
+	/**
+	 * Returns the Edit Distance of the two strings
+	 * 
+	 * @param s1 - First String to compare
+	 * @param s2 - Second String to compare
+	 * @return Edit Distance of the two strings
+	 */
+	public static int getDistance(String s1, String s2)
+	{
+		int len1 = s1.length() + 1;
+		int len2 = s2.length() + 1;
+		int[][] ed = new int[len1][len2];
 
-        // Loop 1 (Always Runs Once)
-        for (int i = 0; i < len1; i++)
-        {
-            ed[i][0] = i;
-        }
-        // Loop 2 (Always Runs Once)
-        for (int j = 0; j < len2; j++)
-        {
-            ed[0][j] = j;
-        }
+		// Loop 1 (Always Runs Once)
+		for (int i = 0; i < len1; i++)
+		{
+			ed[i][0] = i;
+		}
+		// Loop 2 (Always Runs Once)
+		for (int j = 0; j < len2; j++)
+		{
+			ed[0][j] = j;
+		}
 
-        // Loop 3
-        for (int i = 1; i < len1; i++)
-        {
-            for (int j = 1; j < len2; j++)
-            {
-                int min = Math.min(1 + ed[i - 1][j], 1 + ed[i][j - 1]);
+		// Loop 3
+		for (int i = 1; i < len1; i++)
+		{
+			for (int j = 1; j < len2; j++)
+			{
+				int min = Math.min(1 + ed[i - 1][j], 1 + ed[i][j - 1]);
 
-                int x = (s1.charAt(i - 1) == s2.charAt(j - 1) ? 0 : 1) + ed[i - 1][j - 1];
-                ed[i][j] = Math.min(min, x);
-            }
-        }
+				int x = (s1.charAt(i - 1) == s2.charAt(j - 1) ? 0 : 1) + ed[i - 1][j - 1];
+				ed[i][j] = Math.min(min, x);
+			}
+		}
 
-        return ed[len1 - 1][len2 - 1];
-    }
+		return ed[len1 - 1][len2 - 1];
+	}
 
 }
