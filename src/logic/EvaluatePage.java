449938cd7b06478d2b9c4c4b542cab6cfcb3e264
JakeVz <jakevz122@gmail.com>
2017-11-29 20:30:49 -0800

    Added text to the empty pages. These will be implemented later

--- src/logic/EvaluatePage.java
+++ src/logic/EvaluatePage.java
@@ -17,16 +17,28 @@ public class EvaluatePage
 
     public static List<JComponentWithLayout> createFrame()
     {
-
+        final GridBagConstraints c = new GridBagConstraints();
         List<JComponentWithLayout> panels = new ArrayList<JComponentWithLayout>(3);
 
         // Panels
         JPanel header = HeaderFactory.createHeader("Evaluate a Course");
         JPanel footer = new JPanel();
+        JPanel main = new JPanel(new GridBagLayout());
 
-        // Makes table scroll-able
+        
+        //Main
+        main.setBackground(new Color(255, 255, 255));
+        JLabel Text = new JLabel("TO BE IMPLEMENTED AT A FUTURE DATE");
+        Text.setFont(Text.getFont().deriveFont(30.0f));
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        main.add(Text, c);
+        
+
+/*        // Makes table scroll-able
         JScrollPane scroll = new JScrollPane();
-
+*/
         // Everything for footer below
         JLabel footText = new JLabel("© 2017 Polyratings Course Edition");
         footText.setForeground(Color.WHITE);
@@ -35,7 +47,7 @@ public class EvaluatePage
 
         // All panels into frame
         panels.add(new JComponentWithLayout(header, BorderLayout.NORTH));
-        panels.add(new JComponentWithLayout(scroll, BorderLayout.CENTER));
+        panels.add(new JComponentWithLayout(main, BorderLayout.CENTER));
         panels.add(new JComponentWithLayout(footer, BorderLayout.SOUTH));
 
         return panels;
