d63852bfadc18d7fc20646f4271bf43ff4bfabea
Collin Wong <cwong109@calpoly.edu>
2017-11-30 21:12:52 -0800

    SonarQube Fixes

--- src/test/TestHomeWithJComponentWithLayoutIntegration.java
+++ src/test/TestHomeWithJComponentWithLayoutIntegration.java
@@ -3,11 +3,10 @@ package test;
 import static org.junit.Assert.*;
 
 import java.awt.BorderLayout;
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.Test;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+
+import org.junit.jupiter.api.Test;
 
 import logic.Home;
 import logic.JComponentWithLayout;
@@ -17,19 +16,17 @@ import logic.JComponentWithLayout;
 public class TestHomeWithJComponentWithLayoutIntegration {
 
     private JComponent component;
-    //private final String borderLayout;
-    private JFrame frame; 
     
 	@Test
 	public void testJComponentCreationBorder() {
-        frame = new JFrame("PolyRatings Course Edition");
+        JFrame frame = new JFrame("PolyRatings Course Edition");
         frame.setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JComponentWithLayout JCWL = new JComponentWithLayout(component, "North");
+		JComponentWithLayout jcwl = new JComponentWithLayout(component, "North");
 		
-		JComponentWithLayout JCWL2 = Home.createFrame(frame).get(0);
+		JComponentWithLayout jcwl2 = Home.createFrame(frame).get(0);
 		
-		assertEquals(JCWL.getBorderLayout(), JCWL2.getBorderLayout());
+		assertEquals(jcwl.getBorderLayout(), jcwl2.getBorderLayout());
 		
 	}
 
